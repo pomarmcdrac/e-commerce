341ff1552fec5f6e275c40c828564b81da3fa8e0
Omar Morales <pomaral@live.com.mx>
2023-06-06 09:31:51 -0600

    Agrego url de back

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-    SERVER_HOST: "http://localhost:1337",
-    API_URL: "http://localhost:1337/api",
+    SERVER_HOST: "https://e-commerce-back.up.railway.app",
+    API_URL: "https://e-commerce-back.up.railway.app/api",
     ENDPOINTS: {
         AUTH: {
             REGISTER: "auth/local/register",
